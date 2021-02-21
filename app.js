13fc54871dcbb6297e3b5d2d92c8fcf209aeea29
calmighted <calmighted@gmail.com>
2021-02-21 21:24:29 +0530

    fixed sliding issues

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.slider img');
-//button
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
+
+let slideIndex = 1;
+showSlides(slideIndex);
 
 
-let counter = 1;
-const size = images[0].clientWidth;
-
-nextBtn.addEventListener('click',()=>{
-    console.log("clicked")
-    if(counter <= 6) return;
-    slider.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-})
 
 
-prevBtn.addEventListener('click',()=>{
-    console.log("clicked")
-    if(counter <= 0) return;
-    slider.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
-})
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
 
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
 
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName('dot');
+    if(n > slides.length){slideIndex = 1}
+    if(n < 1){slideIndex = slides.length}
+    for(i = 0;i < slides.length;i++){
+        slides[i].style.display = "none";
+    }
+    for(i = 0; i< dots.length;i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
